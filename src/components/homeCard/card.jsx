0b278dccafdb4a881b87b89b9409d31cc3219cf0
BenMc123123 <mcdonaldben02@gmail.com>
2024-08-06 19:55:10 +0100

    Need to do the card stuff now

--- src/components/homeCard/card.jsx
+++ src/components/homeCard/card.jsx
@@ -1,8 +1,14 @@
+import "./cardStyles.css"
+
 
 function Card(props) {
+
     return (
         <>
-        <div className="card-container">
+        <br></br>
+        <div className="card-container" style={{paddingBottom: props.height + "px",
+                                                width: props.width + "%",
+        }}>
             <h1>{ props.title }</h1>
             <p>{ props.mainText }</p>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,23 @@
 import Header from "../components/homeHeader/header"
 import Card from "../components/homeCard/card"
 import "./homeStyles.css"
+
 function Home() {
     return (
         <>
             <Header/>
-            <Card title="Test" mainText="test23" />
+            <div className="card1">
+                <Card title="Card1" mainText="test23" height={231} width={80}/>
+            </div>
+            <div className="cardTop">
+                <Card title="Card2" mainText="test23" height={231} width={80}/>
+            </div>
+            <div className="cardTop">
+                <Card title="Card3" mainText="test23" height={231} width={80}/>
+            </div>
+            <div className="cardTop">
+                <Card title="Card4" mainText="test23" height={231} width={80}/>
+            </div>
         </>
     );
 }
