cef397cfe618f6321f9cd767a943d0f58ad9105f
BenMc123123 <mcdonaldben02@gmail.com>
2024-08-06 11:19:57 +0100

    Home page image + text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import rafac from "../assets/rafac.png"
+import sqn_photo from "../assets/sqn_photo.JPG"
+import "./styles.css"
+
 function Home() {
     return (
-        <h1>Home</h1>
+        <>
+        <div className="background-container">
+            <img src={sqn_photo} alt="Background Image" className="background-image"/>
+            <div className="overlay-image-container">
+                <img src={rafac} alt="Logo" className="overlay-image"/>
+            </div>
+            <div className="text-container">
+                <h1 className="test">317 Newton Heath & <br/>Failsworth Squadron</h1>
+                <button>Join Us</button>
+            </div>
+        </div>
+
+        </>
     );
 }
 
