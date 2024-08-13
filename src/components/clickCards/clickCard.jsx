b422edc14d29f7aa548fc5fee3d62948505faa68
BenMc123123 <mcdonaldben02@gmail.com>
2024-08-13 21:15:58 +0100

    CLick card done, just need to add text and images

--- src/components/clickCards/clickCard.jsx
+++ src/components/clickCards/clickCard.jsx
@@ -1,26 +1,43 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
+
 import "./clickStyles.css"
 
 import Parade from "../../assets/parade.jpeg"
+import Drill from "../../assets/317_drill.png"
+import sqn from "../../assets/sqn_photo.JPG"
 
 
 function clickCard(props) {
+    const images = [Parade, Drill, sqn]
+
+    const [activeElement, setActiveElement] = useState(0);
+
+    const updateActiveElement = (id) => {
+        if (activeElement !== id) {
+            setActiveElement(id);
+        }
+    }
 
     return (
         <>
-        <div>
+        <div className="click-card-container">
             <div className="image">
-                <img src={props.imageSrc}></img>
+                <img src={images[activeElement]}></img>
             </div>
-            <div className="text">
-                <h1 style={{borderLeft: "1px solid",}}>{ props.title }</h1>
-                <p>{ props.mainText }</p>
-                <h1>{ props.title }</h1>
-                <p>{ props.mainText }</p>
-                <h1>{ props.title }</h1>
-                <p>{ props.mainText }</p>
-                <h1>{ props.title }</h1>
-                <p>{ props.mainText }</p>
+            <div className="click-card-text-container">
+                <div onClick={() => updateActiveElement(0)} className={`text ${activeElement === 0 ? "activeCard" : ""}`}>
+                    <h1>{ props.title }</h1>
+                    <p>{ props.mainText }</p>
+                </div>
+                <div onClick={() => updateActiveElement(1)} className={`text ${activeElement === 1 ? "activeCard" : ""}`}>
+                    <h1>{ props.title }</h1>
+                    <p>{ props.mainText }</p>
+                </div>
+                <div onClick={() => updateActiveElement(2)} className={`text ${activeElement === 2 ? "activeCard" : ""}`}>
+                    <h1>{ props.title }</h1>
+                    <p>{ props.mainText }</p>
+                </div>
             </div>
         </div>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,12 +8,12 @@ function Home() {
     return (
         <>
             <Header/>
-            <div className="cards">
+            {/* <div className="cards">
                 <Card secondClass="top-card" title="About Us" mainText="Here at 317 atc, we develop people for their future, we do this by providing them with hard and soft skills. Some of these skills apply to everything in life like teamwork and leadership, We also team things that are more specific such as flying or shooting." height={230} width={80}/>
                 <Card secondClass="left-long-card" title="Activities" mainText="The Air Cadets offer a wide range of activities including flying, shooting, RAF visits, adventure training, DofE and many more." height={645} width={30}/>
                 <Card secondClass="right" title="Aviation" mainText="The Air Cadets has 2 types of aircraft which is available for the cadets to fly. We fly the Grob Tutor T1, which is a propellor powered aircraft and the Viking T1 which is an unpowered glider." height={250} width={48}/>
                 <Card secondClass="right" title="Volunteers" mainText="Adult volunteers are vital to the Air Cadets. Our volunteers serve as role models, instructors, and leaders, guiding cadets in developing their skills and achieving their potential." height={250} width={48}/>
-            </div>
+            </div> */}
             
             <ClickCard/>
         </>
