7a491ae83a5df615c1e0666868b71e117f80ff3c
BenMcD23 <mcdonaldben02@gmail.com>
2024-08-27 12:43:17 +0100

    Header done

--- src/components/homeHeader/header.jsx
+++ src/components/homeHeader/header.jsx
@@ -5,16 +5,20 @@ import "./headerStyles.css"
 function Header() {
     return (
         <>
-        <div className="background-container">
+        <div className="header-container">
             <img src={sqn_photo} alt="Background Image" className="background-image w-100"/>
-            <div className="overlay-image-container">
-                <img src={rafac} alt="Logo" className="overlay-image"/>
-            </div>
-            <div className="text-container">
-                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-5xl text-white">317 Newton Heath & <br/>Failsworth Squadron</h1>
+
+            <div className="header-content grid grid-rows-4 gap-8 md:grid-cols-1 lg:grid-cols-8">
+                <div className="overlay-image-container lg:col-start-2 lg:col-span-2 pt-4">
+                    <img src={rafac} alt="Logo" className="overlay-image"/>
+                </div>
+                <div className="text-container">
+                    <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-5xl text-white">
+                        317 Newton Heath & <br/>Failsworth Squadron
+                    </h1>
+                </div>
             </div>
         </div>
-
         </>
     );
 }
