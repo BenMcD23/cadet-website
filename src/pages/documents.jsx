3e0c9b1781af1118905c429d903e4add6d9a79d2
BenMcD23 <mcdonaldben02@gmail.com>
2024-08-31 14:18:10 +0100

    Docs table

--- src/pages/documents.jsx
+++ src/pages/documents.jsx
@@ -9,23 +9,46 @@ function Documents() {
             <div className="bg-dark-blue-main title-hover text-center pb-3 lg:pt-14">
                 <Title title="Documents"></Title>
             </div>
-            <div className="grid grid-cols-1 text-center">
-                <h1>Forms</h1>
-                <div className="max-md:hidden grid grid-cols-3 text-center">
 
-                    <h1>Form Name</h1>
-                    <h1>When Needed?</h1>
-                    <h1>Download</h1>
-                </div>
-                <div className="grid max-md:grid-cols-1 grid-cols-3 text-center">
-                    <h3>Aviation Medical Form</h3>
-                    <p>Anytime your going flying or gliding</p>
-                    <svg xmlns="http://www.w3.org/2000/svg" className="size-10 m-auto cursor-pointer" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
-                    </svg>
-                </div>
+            <h1>Forms</h1>
+            <table class="docs-table collapsed-table" >
+                    <thead>
+                            <tr>
+                                <th scope="col">Form Name</th>
+                                <th scope="col">Needed When?</th>
+                                <th scope="col">Download</th>
+                            </tr>
+                    </thead>
+                    <tbody>
+                            <tr>
+                                <td data-th="Form Name">Aviation Medical Form</td>
+                                <td data-th="Needed When?">Anytime your going flying or gliding</td>
+                                <td data-th="Download">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-10 m-auto cursor-pointer" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                    </svg>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td data-th="Form Name">TG 21 - Consent Form</td>
+                                <td data-th="Needed When?">Anytime going on any out of Squadron activity</td>
+                                <td data-th="Download">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-10 m-auto cursor-pointer" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                    </svg>
+                                </td>                            </tr>
+                            <tr>
+                                <td data-th="Form Name">TG 23 - Medical Form</td>
+                                <td data-th="Needed When?">Anytime you need a TG 21 and if you have medical condition</td>
+                                <td data-th="Download">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-10 m-auto cursor-pointer" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+                                    </svg>
+                                </td>                            </tr>
+                    </tbody>
+            </table>
 
-            </div>
+
             <SideMenu></SideMenu>
 
         </div>
