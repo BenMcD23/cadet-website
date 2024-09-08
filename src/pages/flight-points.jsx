db555a903c4e9911a9f5c7ede17f2106bf783df6
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-08 17:08:41 +0100

    Fixed flightpoints error

--- src/pages/flight-points.jsx
+++ src/pages/flight-points.jsx
@@ -2,20 +2,20 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Title from "../components/mainTitle/title";
 
-function FlightPoints() {
-    const [csvData, setCsvData] = useState([]);
+function Flightpoints() {
+    const [pointsData, setpointsData] = useState([]);
 
     useEffect(() => {
         fetchCSVData();    // Fetch the CSV data when the component mounts
-    }, []); // The empty array ensures that this effect runs only once, like componentDidMount
+    }, []);
 
     const fetchCSVData = () => {
-        const csvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPqhm8v6F0T2M1Q96MMhSQEw5Jat4oU6-fqgY0USCcppYxjcpRgCSYG1vzNFhdbLfPyRZd65gf1ybT/pub?output=csv"; // Replace with your Google Sheets CSV URL
+        const csvUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQPqhm8v6F0T2M1Q96MMhSQEw5Jat4oU6-fqgY0USCcppYxjcpRgCSYG1vzNFhdbLfPyRZd65gf1ybT/pub?output=csv";
 
-        axios.get(csvUrl)    // Use Axios to fetch the CSV data
+        axios.get(csvUrl)
             .then((response) => {
-                const parsedCsvData = parseCSV(response.data);  // Parse the CSV data into an array of objects
-                setCsvData(parsedCsvData);  // Set the fetched data in the component's state
+                const parsedCsvData = parseCSV(response.data);
+                setpointsData(parsedCsvData);  // set fetched data
             })
             .catch((error) => {
                 console.error('Error fetching CSV data:', error);
@@ -43,15 +43,15 @@ function FlightPoints() {
             <div className="grid grid-cols-2 text-center m-auto w-1/2 pt-3">
                 <div>
                     <h1 className="text-8xl font-extrabold">A Flight</h1>
-                    <h2 className="text-5xl pt-4 font-bold">{csvData[0]}</h2> {/* Use csvData state */}
+                    <h2 className="text-5xl pt-4 font-bold">{pointsData[0]}</h2>
                 </div>
                 <div>
                     <h1 className="text-8xl font-extrabold">B Flight</h1>
-                    <h2 className="text-5xl pt-4 font-bold">{csvData[1]}</h2> {/* Use csvData state */}
+                    <h2 className="text-5xl pt-4 font-bold">{pointsData[1]}</h2>
                 </div>
             </div>
         </>
     );
 }
 
-export default FlightPoints;
+export default Flightpoints;
