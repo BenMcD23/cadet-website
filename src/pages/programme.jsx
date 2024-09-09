ea4b6aa55b048014b1aefc4a410f8b80150fe6e1
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-09 22:37:40 +0100

    Added title to programme

--- src/pages/programme.jsx
+++ src/pages/programme.jsx
@@ -1,12 +1,19 @@
 import Programme_png from "/programme.png";
+import Title from "../components/mainTitle/title";
+
 
 function Programme() {
     return (
-        <div className="lg:pt-20">
-            <a href="/programme.pdf" download="Programme.pdf">
-                <img src={Programme_png} className="m-auto max-h-screen max-w-screen object-contain" alt="Programme" />
-            </a>
+        <>
+        <div className="bg-dark-blue-main title-hover text-center pb-3 lg:pt-14">
+            <Title title="Programme"></Title>
         </div>
+
+        <a href="/programme.pdf" download="Programme.pdf">
+            <img src={Programme_png} className="m-auto max-h-screen max-w-screen object-contain" alt="Programme" />
+        </a>
+
+        </>
     );
 }
 
