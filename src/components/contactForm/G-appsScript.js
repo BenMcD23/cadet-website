b083aa86a8f3f601ed7a12025c79dee42c011e39
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-09 14:12:02 +0100

    Captcha added and sanitisation on app scripts

--- src/components/contactForm/G-appsScript.js
+++ src/components/contactForm/G-appsScript.js
@@ -2,6 +2,11 @@
 
 function doPost(e) {
   const data = e.parameter;
+  
+  // Basic validation
+  if (!data.Name || !data.Email || !data.Contact_Reason || !data.Message) {
+    return ContentService.createTextOutput(JSON.stringify({ 'result': 'error', 'message': 'Invalid input' })).setMimeType(ContentService.MimeType.JSON);
+  }
 
   submitFormData(data);
   return ContentService.createTextOutput(JSON.stringify({ 'result': 'success' })).setMimeType(ContentService.MimeType.JSON);
@@ -9,28 +14,41 @@ function doPost(e) {
 
 function submitFormData(data) {
   const timestamp = new Date();
+  const spreadsheetId = '1Yv3JoNhQG8MjZ8Mgj0SFvgLkPszagoZ1kTf4hxTpqKc'; 
 
-  const spreadsheetId = '1Yv3JoNhQG8MjZ8Mgj0SFvgLkPszagoZ1kTf4hxTpqKc';  // Use the Spreadsheet ID
-
-  // Open the spreadsheet by ID
   const sheet = SpreadsheetApp.openById(spreadsheetId).getActiveSheet();
 
-  sheet.appendRow([timestamp, data.Name, data.Email, data.Contact_Reason, data.Message]);
+  // Basic sanitization
+  const sanitizedName = sanitizeInput(data.Name);
+  const sanitizedEmail = sanitizeInput(data.Email);
+  const sanitizedContactReason = sanitizeInput(data.Contact_Reason);
+  const sanitizedMessage = sanitizeInput(data.Message);
 
-  // send email
+  sheet.appendRow([timestamp, sanitizedName, sanitizedEmail, sanitizedContactReason, sanitizedMessage]);
+
   MailApp.sendEmail({
     to: "mcdonaldben02@gmail.com",
-    replyTo: data.Email,
+    replyTo: sanitizedEmail,
     subject: "Contact Form Submission",
     htmlBody: `
       <p>The website's contact form has been filled out</p>
-      <h2>Name:</h2><p>${data.Name}</p>
-      <h2>Contact Reason:</h2><p>${data.Contact_Reason}</p>
-      <h2>Message:</h2><p>${data.Message}</p>
+      <h2>Name:</h2><p>${sanitizedName}</p>
+      <h2>Contact Reason:</h2><p>${sanitizedContactReason}</p>
+      <h2>Message:</h2><p>${sanitizedMessage}</p>
     `
   });
-    
-  }
+}
 
-
+function sanitizeInput(input) {
+  const map = {
+      '&': '&amp;',
+      '<': '&lt;',
+      '>': '&gt;',
+      '"': '&quot;',
+      "'": '&#x27;',
+      "/": '&#x2F;',
+  };
+  const reg = /[&<>"'/]/ig;
+  return input.replace(reg, (match)=>(map[match]));
+}
 
--- src/components/contactForm/contactForm.jsx
+++ src/components/contactForm/contactForm.jsx
@@ -1,20 +1,33 @@
-
 import React, { useState, useRef } from "react";
-
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
-import "./formStyles.css"
+import ReCAPTCHA from 'react-google-recaptcha';
+import "./formStyles.css";
 
 function ContactForm() {
-  const [successMessage, setSuccessMessage] = useState(""); // State for success message
+  const [successMessage, setSuccessMessage] = useState("");
+  const [captchaMessage, setCaptchaMessage] = useState("");
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
+  // for changing button to submitting so can spam
+  const [isSubmitting, setIsSubmitting] = useState(false);
   const formRef = useRef(null);
 
+  const handleRecaptchaChange = (value) => {
+    setRecaptchaValue(value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    
+    if (!recaptchaValue) {
+      setCaptchaMessage("Complete captcha to submit message");
+      return;
+    }
 
-    fetch("https://script.google.com/macros/s/AKfycbyrgt7R07BlvIAuCIw7tMDtoYgCI8vyyQ97RLtNcAmu7Sq1_in3zHfso0PJS53r0k9KNA/exec", {
+    setCaptchaMessage("");
+    setIsSubmitting(true);
+
+    fetch("https://script.google.com/macros/s/AKfycbxPeL-ERFdWTypczMm65P-j9dDrT5_xW-Q2_Dw6cWy7_on_wW4jxKuljaT1PtoRD5x3qw/exec", {
       method: 'POST',
       body: new FormData(formRef.current),
     })
@@ -23,6 +36,7 @@ function ContactForm() {
       if (data.result === 'success') {
         setSuccessMessage("Your message has been sent successfully!");
         formRef.current.reset();
+        setRecaptchaValue(null);
       } else {
         setSuccessMessage("There was an error sending your message. Please try again.");
       }
@@ -30,27 +44,28 @@ function ContactForm() {
     .catch(err => {
       console.error(err);
       setSuccessMessage("There was an error sending your message. Please try again.");
+    })
+    .finally(() => {
+      setIsSubmitting(false);
     });
   };
 
-
   return (
-      <Form 
-        className="contact-form"
-        ref={formRef} 
-        onSubmit={handleSubmit}
-      >
-        <h3 className="text-center text-3xl font-extrabold leading-none tracking-tight md:text-4xl dark:text-white">Contact Form</h3>
-        <Form.Group className="mb-3" controlId="formName">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Full Name" name="Name" required />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" name="Email" required />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formInterest">
+    <Form 
+      className="contact-form"
+      ref={formRef} 
+      onSubmit={handleSubmit}
+    >
+      <h3 className="text-center text-3xl font-extrabold leading-none tracking-tight md:text-4xl dark:text-white">Contact Form</h3>
+      <Form.Group className="mb-3" controlId="formName">
+        <Form.Label>Full Name</Form.Label>
+        <Form.Control type="text" placeholder="Enter Full Name" name="Name" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" name="Email" required />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formInterest">
         <Form.Label>Contact Reason</Form.Label>
         <Form.Select name="Contact_Reason" required>
           <option value="Cadet joining interest">Cadet joining interest</option>
@@ -58,25 +73,30 @@ function ContactForm() {
           <option value="General enquiry">General enquiry</option>
         </Form.Select>
       </Form.Group>
-
-        <Form.Group className="mb-3" controlId="Description">
-          <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={4} name="Message" required />
-        </Form.Group>
-        <div className="text-center">
+      <Form.Group className="mb-3" controlId="Description">
+        <Form.Label>Message</Form.Label>
+        <Form.Control as="textarea" rows={4} name="Message" required />
+      </Form.Group>
+      <Form.Group className="mb-3 flex justify-center">
+        <ReCAPTCHA
+          sitekey="6LenpjsqAAAAAL2_JNQZC4WmIjOf1llhhqsxufWI" 
+          onChange={handleRecaptchaChange}
+        />
+      </Form.Group>
+      <div className="text-center">
         <Button 
-        className="shift-button"
-        variant="success" 
-        type="submit"
-      >
-        Submit
-      </Button>
-          {successMessage && <div className="text-green-500	text-lg font-semibold pt-2">{successMessage}</div>}
-
-        </div>
-      </Form>
-
+          className="shift-button"
+          variant="success" 
+          type="submit"
+          disabled={isSubmitting} // Disable button when submitting
+        >
+          {isSubmitting ? "Submitting..." : "Submit"} {/* Change button text when submitting */}
+        </Button>
+        {successMessage && <div className="text-green-500 text-lg font-semibold pt-2">{successMessage}</div>}
+        {captchaMessage && <div className="text-red-500 text-lg font-semibold pt-2">{captchaMessage}</div>}
+      </div>
+    </Form>
   );
-};
+}
 
 export default ContactForm;
