8dd2ee74793a7d1e8ef71da7c32d41e034a1633d
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-10 21:28:20 +0100

    Added vercel insights

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,16 @@ import Parents from './pages/parents'
 import { Route, Routes } from "react-router-dom"
 import './App.css'
 
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 function App() {
   return (
     
     <>
+    <Analytics/>
+    <SpeedInsights/>
+    
     <div className="app-container">
       <Navbar/>
       <ScrollToTop>
