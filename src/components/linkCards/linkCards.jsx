37884e36879537b915b261e6736c1dedd065dcf1
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-10 16:42:14 +0100

    Images for link cards

--- src/components/linkCards/linkCards.jsx
+++ src/components/linkCards/linkCards.jsx
@@ -1,5 +1,11 @@
 import "./linkCardStyles.css";
 import Title from "../mainTitle/title";
+import { Link } from "react-router-dom";
+
+import Programmes from "../../assets/home_images/programmes.png";
+import Contact from "../../assets/home_images/contact.png";
+import Facebook from "../../assets/home_images/facebook.png";
+
 import drill from "../../assets/317_drill.png";
 
 function LinkCards() {
@@ -9,24 +15,23 @@ function LinkCards() {
 
             <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-4 middle:grid-cols-3 w-3/4 mx-auto content-center text-center mt-20">
                 {data.map((d, index) => (
-                    <div
-                        key={d.title}
-                        className={`link-card relative ${index === 2 ? 'md:col-start-2 md:col-end-4 middle:col-start-3' : 'md:col-span-2 middle:col-span-1'}`}
-                    >
-                        <img className="link-card-img w-full" src={d.img} alt={d.title} />
-                        
-                        <div className="absolute top-2 left-1/2 transform -translate-x-1/2 text-white text-xl font-bold">
-                            <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-5xl dark:text-white">{d.title}</h1>
-                        </div>
+                    <Link to={d.link}>
+                        <div
+                            key={d.title}
+                            className={`link-card relative ${index === 2 ? 'md:col-start-2 md:col-end-4 middle:col-start-3' : 'md:col-span-2 middle:col-span-1'}`}
+                        >
+                            <img className="link-card-img w-full" src={d.img} alt={d.title} />
+                            
+                            <div className="absolute top-2 left-1/2 transform -translate-x-1/2 text-white text-xl font-bold">
+                                <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-5xl text-white">{d.title}</h1>
+                            </div>
 
-                        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
-                            <p className="mb-6 font-normal lg:text-xl text-center dark:text-white">{d.main}</p>
+                            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
+                                <p className="mb-6 font-normal lg:text-xl text-center dark:text-white">{d.main}</p>
+                            </div>
                         </div>
+                    </Link>
 
-                        <a href={d.link} className="link-card-link absolute bottom-4 left-1/2 transform -translate-x-1/2 text-white">
-                            Learn More
-                        </a>
-                    </div>
                 ))}
             </div>
         </div>
@@ -35,22 +40,22 @@ function LinkCards() {
 
 const data = [
     {
+        title: `Contact Us`,
+        img: Contact,
+        main: `Contact form for any enqiures`,
+        link: `/contact`
+    },
+    {
         title: `Programme`,
-        img: drill,
+        img: Programmes,
         main: `See what we're up to this month`,
-        link: ``
+        link: `/programme`
     },
     {
-        title: `Meet the Staff`,
-        img: drill,
-        main: `Our dedicated volunteers who help run the squadron every night and organise exciting events and training`,
-        link: ``
-    },
-    {
-        title: `Our Cadet NCO's`,
-        img: drill,
-        main: `Our cadet NCO team run sessions each night and are responsible for the organisation of every parade night.`,
-        link: ``
+        title: `Our Facebook`,
+        img: Facebook,
+        main: `Stay updated with all our latest news and events`,
+        link: `https://www.facebook.com/317atc`
     },
 ];
 
