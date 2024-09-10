050707fc884f0dd4b1bd06a24ebeadfe3587a82c
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-10 19:24:21 +0100

    Changed flight points to session storage

--- src/context/flightPointsContext.jsx
+++ src/context/flightPointsContext.jsx
@@ -5,20 +5,20 @@ const PointsContext = createContext();
 export const FlightPointsProvider = ({ children }) => {
     const [pointsData, setPointsData] = useState(() => {
         // Try to load data from localStorage initially
-        const storedData = localStorage.getItem('pointsData');
+        const storedData = sessionStorage.getItem('pointsData');
         return storedData ? JSON.parse(storedData) : [];
     });
 
     useEffect(() => {
         // check if data is already in local storage, if not, fetch it
-        if (!localStorage.getItem('pointsData')) {
+        if (!sessionStorage.getItem('pointsData')) {
             fetch("https://script.google.com/macros/s/AKfycbyrzezJIa_vvlhbXWu8v1NDOHXD9w8DcAv_37OOvslkUYHRploStFdOSJOTzbDLHndjpQ/exec", {
                 method: 'POST'
             })
             .then(response => response.json())
             .then(data => {
                 setPointsData(data);
-                localStorage.setItem('pointsData', JSON.stringify(data)); // cache
+                sessionStorage.setItem('pointsData', JSON.stringify(data)); // cache
             })
             .catch(error => {
                 console.error('Error fetching data:', error);
