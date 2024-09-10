2fa5b853050954db384477b1378bf044cfe9b2a0
BenMcD23 <mcdonaldben02@gmail.com>
2024-09-10 18:31:24 +0100

    Few small changes

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -125,11 +125,7 @@ function Footer() {
                                 <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                                 <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                             </svg>
-                            adj.317@rafac.mod.gov.uk
-                            <div className='ml-2'>|</div>
-                            <Link className="ml-2 items-center text-neutral-200" to="/contact">
-                                Contact Form
-                            </Link>
+                            adj.317@rafac.mod.gov.uk                        
                         </p>
                         <p className="mb-2 flex items-center justify-center lg:justify-start">
                             <svg
--- src/components/linkCards/linkCards.jsx
+++ src/components/linkCards/linkCards.jsx
@@ -13,13 +13,14 @@ function LinkCards() {
         <div className="link-card-container title-hover clip-path-sm-l-r md:clip-path-md-l-r lg:clip-path-lg-l-r">
             <Title title="Quick Links"></Title>
 
-            <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-4 middle:grid-cols-3 w-3/4 mx-auto content-center text-center mt-20">
+            <div className="grid gap-8 sm:grid-cols-1 md:grid-cols-4 middle:grid-cols-3 w-3/4 mx-auto content-center text-center mt-10">
                 {data.map((d, index) => (
-                    <Link to={d.link}>
                         <div
                             key={d.title}
                             className={`link-card relative ${index === 2 ? 'md:col-start-2 md:col-end-4 middle:col-start-3' : 'md:col-span-2 middle:col-span-1'}`}
                         >
+                        <Link to={d.link}>
+
                             <img className="link-card-img w-full" src={d.img} alt={d.title} />
                             
                             <div className="absolute top-2 left-1/2 transform -translate-x-1/2 text-white text-xl font-bold">
@@ -29,8 +30,9 @@ function LinkCards() {
                             <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full px-4">
                                 <p className="mb-6 font-normal lg:text-xl text-center dark:text-white">{d.main}</p>
                             </div>
+                        </Link>
+
                         </div>
-                    </Link>
 
                 ))}
             </div>
