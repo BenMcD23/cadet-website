7dc12e4e5bc0b2be32382fb96bcc45dfc70bbf45
BenMcD23 <mcdonaldben02@gmail.com>
2024-10-24 22:26:31 +0100

    Staff and nco pics now just in rows, not hierarchical

--- src/components/ncoPics/ncoPics.jsx
+++ src/components/ncoPics/ncoPics.jsx
@@ -19,106 +19,76 @@ function NCOPics() {
     return (
         <>
 
-        <div className="grid grid-cols-1 xl:grid-cols-14 grid-rows-3 gap-1 staff-container pt-4">
-            {/* Top Row (1 person) */}
-            <div className="flex items-center justify-center xl:col-start-7 xl:col-end-9 xl:row-start-1 xl:row-end-2">
-                <div className="image-text-container text-center">
-                    <img src={CWO_Lloyd_Morris} className="staff-img" alt="Flt Lt Simon Doherty" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">CWO G Lloyd Morris</p>
-                </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-1 staff-container pt-4">
+            <div className="image-text-container text-center">
+                <img src={CWO_Lloyd_Morris} className="staff-img m-auto" alt="CWO G Lloyd Morris" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">CWO G Lloyd Morris</p>
             </div>
 
-            {/* Second Row (6 people) */}
-            <div className="flex items-center justify-center xl:col-start-2 xl:col-end-4 xl:row-start-2 xl:row-end-3">
-                <div className="image-text-container text-center">
-                    <img src={Sgt_Bardsley} className="staff-img" alt="CI Roy Catterall" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt A Bardsley</p>
-                </div>
+    
+            <div className="image-text-container text-center">
+                <img src={Sgt_Bardsley} className="staff-img m-auto" alt="Sgt A Bardsley" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt A Bardsley</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-4 xl:col-end-6 xl:row-start-2 xl:row-end-3">
-                <div className="image-text-container text-center">
-                    <img src={Sgt_Murray} className="staff-img" alt="WO Jonathon Barker" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt C Murray</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Sgt_Murray} className="staff-img m-auto" alt="Sgt C Murray" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt C Murray</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-6 xl:col-end-8 xl:row-start-2 xl:row-end-3">
-                <div className="image-text-container text-center">
-                    <img src={Sgt_Evans} className="staff-img" alt="FS Joseph Gill" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt K Evans</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Sgt_Evans} className="staff-img m-auto" alt="Sgt K Evans" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt K Evans</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-8 xl:col-end-10 xl:row-start-2 xl:row-end-3">
-                <div className="image-text-container text-center">
-                    <img src={Sgt_Hall} className="staff-img" alt="CI Ben McDonald" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt L Hall</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Sgt_Hall} className="staff-img m-auto" alt="Sgt L Hall" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt L Hall</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-10 xl:col-end-12 xl:row-start-2 xl:row-end-3">
-                <div className="image-text-container text-center">
-                    <img src={Sgt_Wimbury} className="staff-img" alt="CI Jacob Gill" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt T Wimbury</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Sgt_Wimbury} className="staff-img m-auto" alt="Sgt T Wimbury" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt T Wimbury</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-12 xl:col-end-[14] xl:row-start-2 xl:row-end-3">
-                <div className="image-text-container text-center">
-                    <img src={Sgt_Tyrell} className="staff-img" alt="CI Sophie Stone" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt V Tyrell</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Sgt_Tyrell} className="staff-img m-auto" alt="Sgt V Tyrell" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Sgt V Tyrell</p>
             </div>
 
-            {/* Third Row (7 people) */}
-            <div className="flex items-center justify-center xl:col-start-1 xl:col-end-3 xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Milligan} className="staff-img" alt="CI Roy Catterall" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl B Milligan</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Milligan} className="staff-img m-auto" alt="Cpl B Milligan" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl B Milligan</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-3 xl:col-end-5 xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Davies} className="staff-img" alt="WO Jonathon Barker" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl G Davies</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Davies} className="staff-img m-auto" alt="Cpl G Davies" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl G Davies</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-5 xl:col-end-7 xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Lear} className="staff-img" alt="FS Joseph Gill" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl J Lear</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Lear} className="staff-img m-auto" alt="Cpl J Lear" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl J Lear</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-7 xl:col-end-9 xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Evans} className="staff-img" alt="CI Ben McDonald" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl K Evans</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Evans} className="staff-img m-auto" alt="Cpl K Evans" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl K Evans</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-9 xl:col-end-11 xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Drury} className="staff-img" alt="CI Jacob Gill" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl L Drury</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Drury} className="staff-img m-auto" alt="Cpl L Drury" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl L Drury</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-11 xl:col-end-13 xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Beverley} className="staff-img" alt="CI Sophie Stone" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl M Beverly</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Beverley} className="staff-img m-auto" alt="Cpl M Beverly" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl M Beverly</p>
             </div>
 
-            <div className="flex items-center justify-center xl:col-start-13 xl:col-end-[15] xl:row-start-3 xl:row-end-4">
-                <div className="image-text-container text-center">
-                    <img src={Cpl_Mullery_McCourt} className="staff-img" alt="CI Roy Catterall" />
-                    <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl N Mullery McCourt</p>
-                </div>
+            <div className="image-text-container text-center">
+                <img src={Cpl_Mullery_McCourt} className="staff-img m-auto" alt="Cpl N Mullery McCourt" />
+                <p className="text-xl font-extrabold leading-none xl:text-2xl text-black">Cpl N Mullery McCourt</p>
             </div>
         </div>
     </>
--- src/components/staffPics/staffPics.jsx
+++ src/components/staffPics/staffPics.jsx
@@ -13,54 +13,42 @@ import "./staffPicsStyles.css";
 function StaffPics() {
     return (
         <>
-            <div className="grid grid-cols-1 md:grid-cols-12 grid-rows-4 gap-1 staff-container pt-4">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-1 staff-container pt-4">
                 
-                <div className="flex items-center justify-center md:col-start-5 md:col-end-9 md:row-start-1 md:row-end-2">
 
-                    <div className="image-text-container text-center">
-                        <img src={Flt_Lt_Doherty} className="staff-img" alt="Flt Lt Simon Doherty" />
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">Flt Lt Simon Doherty</p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center md:col-start-5 md:col-end-9 md:row-start-2 md:row-end-3">
-                    <div className="image-text-container text-center">
-                        <img src={CI_Catterall} className="staff-img" alt="CI Roy Catterall" />
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Roy Catterall (Wg Cdr Ret'd)</p>
-                    </div>
+                <div className="image-text-container text-center">
+                    <img src={Flt_Lt_Doherty} className="staff-img m-auto" alt="Flt Lt Simon Doherty" />
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">Flt Lt Simon Doherty</p>
                 </div>
 
-                <div className="flex items-center justify-center  md:col-start-3 md:col-end-7 md:row-start-3 md:row-end-4">
-                    <div className="image-text-container text-center">
-                        <img src={WO_Barker} className="staff-img" alt="WO Jonathon Barker" />
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">WO Jonathon Barker</p>
-                    </div>
+                <div className="image-text-container text-center">
+                    <img src={CI_Catterall} className="staff-img m-auto" alt="CI Roy Catterall" />
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Roy Catterall (Wg Cdr Ret'd)</p>
                 </div>
 
-                <div className="flex items-center justify-center md:col-start-7 md:col-end-11 md:row-start-3 md:row-end-4">
-                    <div className="image-text-container text-center">
-                        <img src={FS_Gill} className="staff-img" alt="FS Joseph Gill" />
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">FS Joseph Gill</p>
-                    </div>
-                </div>
-                <div className="flex items-center justify-center md:col-start-1 md:col-end-5 md:row-start-4 md:row-end-5">
-                    <div className="image-text-container text-center">
-                        <img src={CI_McDonld} className="staff-img" alt="CI Ben McDonald" />
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Ben McDonald</p>
-                    </div>
+                <div className="image-text-container text-center">
+                    <img src={WO_Barker} className="staff-img m-auto" alt="WO Jonathon Barker" />
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">WO Jonathon Barker</p>
                 </div>
 
-                <div className="flex items-center justify-center md:col-start-5 md:col-end-9 md:row-start-4 md:row-end-5">
-                    <div className="image-text-container m-auto">
-                        <img src={CI_Gill} className="staff-img"/>
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Jacob Gill</p>
-                    </div>
+                <div className="image-text-container text-center">
+                    <img src={FS_Gill} className="staff-img m-auto" alt="FS Joseph Gill" />
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">FS Joseph Gill</p>
                 </div>
 
-                <div className="flex items-center justify-center md:col-start-9 md:col-end-13 md:row-start-4 md:row-end-5">
-                    <div className="image-text-container m-auto">
-                        <img src={CI_Stone} className="staff-img"/>
-                        <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Sophie Stone</p>
-                    </div>
+                <div className="image-text-container text-center">
+                    <img src={CI_McDonld} className="staff-img m-auto" alt="CI Ben McDonald" />
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Ben McDonald</p>
+                </div>
+
+                <div className="image-text-container text-center">
+                    <img src={CI_Gill} className="staff-img m-auto"/>
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Jacob Gill</p>
+                </div>
+
+                <div className="image-text-container text-center">
+                    <img src={CI_Stone} className="staff-img m-auto"/>
+                    <p className="text-xl font-extrabold leading-none lg:text-2xl text-black">CI Sophie Stone</p>
                 </div>
             </div>
         </>
