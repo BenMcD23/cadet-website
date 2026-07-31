4d8337d2640a54102d1167445ea6e87111c9f168
BenMcD23 <mcdonaldben02@gmail.com>
2026-07-31 11:43:15 +0100

    update pics

--- src/components/ncoPics/ncoPics.jsx
+++ src/components/ncoPics/ncoPics.jsx
@@ -7,8 +7,6 @@ const getImage = (fileName) =>
   images[`../../assets/nco_pics/${fileName}`]?.default;
 
 const ncos = [
-    { name: "CWO V Tyrell", alt: "CWO V Tyrell", image: "CWO Tyrell.webp" },
-
     { name: "FS M Beverley", alt: "FS M Beverley", image: "FS Beverley.webp" },
     { name: "FS N Mullery-McCourt", alt: "FS N Mullery-McCourt", image: "FS Mullery-McCourt.webp" },
     { name: "FS T Wimbury", alt: "FS T Wimbury", image: "FS Wimbury.webp" },
@@ -17,12 +15,15 @@ const ncos = [
     { name: "Sgt E Tyrell", alt: "Sgt E Tyrell", image: "Sgt Tyrell.webp" },
     { name: "Sgt F Malik", alt: "Sgt F Malik", image: "Cpl Malik.webp" },
     { name: "Sgt G Davies", alt: "Sgt G Davies", image: "Sgt Davies.webp" },
+    { name: "Sgt T Tyrell", alt: "Sgt T Tyrell", image: "Cpl Tyrell.webp" },
 
     { name: "Cpl H Boxall", alt: "Cpl H Boxall", image: "Cdt Boxall.webp" },
+    { name: "Cpl I Shahbaz", alt: "Cpl I Shahbaz", image: "Cdt Shahbaz I.webp" },
     { name: "Cpl I Wiggett", alt: "Cpl I Wiggett", image: "Cdt Wiggett.webp" },
+    { name: "Cpl O Slack", alt: "Cpl O Slack", image: "Cdt Slack.webp" },
     { name: "Cpl R Sawczuk", alt: "Cpl R Sawczuk", image: "Cdt Sawczuk.webp" },
+    { name: "Cpl S Okon", alt: "Cpl S Okon", image: "Cdt Okon.webp" },
     { name: "Cpl T Partington", alt: "Cpl T Partington", image: "Cpl Partington.webp" },
-    { name: "Cpl T Tyrell", alt: "Cpl T Tyrell", image: "Cpl Tyrell.webp" },
 ];
 
 function NCOPics() {
